26ea953b8bb6cd905d72b99a4bef655ba36fbe2c
enriri <44953034+enriri@users.noreply.github.com>
2019-09-30 00:57:41 -0300

    v0.2
    - Added Cadastro ( Animation, Layout, progession bar, transition steps )
    - Changed Navbav (layout , items)
    - Changed footer ( text )
    - Added Jquery lib
    - Added popper.js lib
    
    Missing:
    - Validation
    - Login view
    - Home content

--- CRUD Angular/CRUDAngular/src/app/carousel/carousel.component.ts
+++ CRUD Angular/CRUDAngular/src/app/carousel/carousel.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as $ from 'jquery';
 
 @Component({
   selector: '[app-carousel]',
@@ -10,6 +11,50 @@ export class CarouselComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+
+    function escode_etapas(){
+      $("#etapa1").hide();
+      $("#etapa2").hide();
+      $("#etapa3").hide();
+    }
+
+    function progresso(prog){
+      $("#progresso").attr("style", "width: "+ prog + "%");
+    }
+
+    escode_etapas();
+    $("#etapa1").toggle();
+
+    $("#etapa1_prox").click(function() {
+      escode_etapas();
+      progresso(66);
+      $("#etapa2").slideToggle();
+      
+    });
+
+    $("#etapa2_ant").click(function (){
+      escode_etapas();
+      progresso(33);
+      $("#etapa1").slideToggle();
+
+    });
+
+    $("#etapa2_prox").click(function (){
+      escode_etapas();
+      progresso(99);
+      $("#etapa3").slideToggle();
+
+    });
+
+    $("#etapa3_ant").click(function (){
+      escode_etapas();
+      progresso(66);
+      $("#etapa2").slideToggle();
+
+    });
+
+    
+
   }
 
 }
